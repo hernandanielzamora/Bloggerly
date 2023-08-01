4661921aad00ca807faef3607f81eeee4adcdc04
Hernan Zamora <hernanzamora1999@gmail.com>
2023-08-01 18:47:12 -0300

    Created most of the devise views - Needs styling - All mailers are working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   devise_for :users
   root "users#index"
+  get 'passwords/edit', to: 'passwords#edit', as: :edit_password
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
