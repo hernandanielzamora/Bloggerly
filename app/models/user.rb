b57ad2d630437e98e721a3d665ff4436bd818440
Hernan Zamora <hernanzamora1999@gmail.com>
2023-08-01 15:37:43 -0300

    Installed Devise and Open_letter to handle log in and confirmation email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
 
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :name, presence: true
+  validates :name, presence: true, allow_blank: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
 
   before_create :set_initial_posts_counter
 
