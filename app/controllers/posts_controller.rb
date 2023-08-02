253e5e595c6d78d4a45acecf4a6680e427d601f5
Hernan Zamora <hernanzamora1999@gmail.com>
2023-08-02 12:24:50 -0300

    Edited comments and posts controllers - Created ability model for cancan

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
+  load_and_authorize_resource
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments, :likes).paginate(page: params[:page], per_page: 2)
@@ -21,6 +24,14 @@ class PostsController < ApplicationController
       flash.now[:alert] = 'An error has occurred while creating the post.'
       render :new
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @author = @post.author
+    @author.decrement!(:posts_counter)
+    @post.destroy!
+    redirect_to user_posts_path(id: @author.id), notice: 'Post deleted!'
   end
 
   private
